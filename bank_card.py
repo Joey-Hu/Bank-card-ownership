ae2e5f1bb6e7304f8064d763d056f9a1616d955b
huhao_MI <1925595006@qq.com>
2020-08-02 16:22:35 +0800

    go newbie

--- bank_card.py
+++ bank_card.py
@@ -8,7 +8,7 @@ import time
 import random
 
 
-# 读取excel
+# read excel file
 data = pd.read_excel(r'D:/software/Desktop/银行卡/not_match_7.xls', usecols=[0])
 # data = pd.read_csv(r'test1.csv')
 data_li = data.values.tolist()
@@ -18,7 +18,7 @@ for s_li in data_li:
 print(data)
 print(len(data))
 
-# 创建excel工作表
+# create worksheet
 workbook = xlwt.Workbook(encoding='utf-8')
 worksheet = workbook.add_sheet('sheet1')
 
@@ -28,29 +28,27 @@ execute_times = 1
 
 for card_num in data:
     print("执行次数"+ str(execute_times))
-    # 暂停10秒
-    time.sleep(2)
     querys = 'bankcard=' + str(card_num)
     print(querys)
-    # 查询
+    # inqury
     host = 'https://jisuyhkgsd.market.alicloudapi.com'
     path = '/bankcard/query'
     method = 'ANY'
     appcode = '8923c88261eb4bc688a3fb60522b0d48'
     bodys = {}
     url = host + path + '?' + querys
-    # 添加headers
-    headers = {}
-    user_agent_list = [
-        "Opera/8.0 (Windows NT 5.1; U; en)",
-        "Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.50",
-        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 9.50",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0",
-        ]
-    headers['User-Agent'] = random.choice(user_agent_list)
+    # # 添加headers
+    # headers = {}
+    # user_agent_list = [
+    #     "Opera/8.0 (Windows NT 5.1; U; en)",
+    #     "Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.50",
+    #     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 9.50",
+    #     "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0",
+    #     ]
+    # headers['User-Agent'] = random.choice(user_agent_list)
     # 添加异常处理
     try:
-        req = urllib.request.Request(url, headers=headers)
+        req = urllib.request.Request(url)
         req.add_header('Authorization', 'APPCODE ' + appcode)
         # //根据API的要求，定义相对应的Content-Type
         req.add_header('Content-Type', 'application/json; charset=UTF-8')
@@ -59,8 +57,8 @@ for card_num in data:
         ctx.verify_mode = ssl.CERT_NONE
         response = request.urlopen(req, context=ctx)
         content = response.read().decode()
-        # 获取到json
-        # 解析json
+        # get json
+        # parse json
         if (content):
             content1 = json.loads(content)
             card_info = content1['result']
