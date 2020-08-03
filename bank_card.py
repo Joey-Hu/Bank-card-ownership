e145e5f8ddcf99e51e8773789a55f3fef56f97fb
huhao_MI <1925595006@qq.com>
2020-08-03 14:56:04 +0800

    go newbie

--- bank_card.py
+++ bank_card.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   bank_card.py
+@Time    :   2020/08/01 14:53:43
+@Author  :   Hu Hao 
+@Version :   1.0
+@Desc    :   None
+'''
 import urllib, sys
 from urllib import request
 import ssl
@@ -9,7 +18,7 @@ import random
 
 
 # read excel file
-data = pd.read_excel(r'D:/software/Desktop/银行卡/not_match_7.xls', usecols=[0])
+data = pd.read_excel(r'./not_match_7.xls', usecols=[0])
 # data = pd.read_csv(r'test1.csv')
 data_li = data.values.tolist()
 data = []
@@ -37,27 +46,16 @@ for card_num in data:
     appcode = '8923c88261eb4bc688a3fb60522b0d48'
     bodys = {}
     url = host + path + '?' + querys
-    # # 添加headers
-    # headers = {}
-    # user_agent_list = [
-    #     "Opera/8.0 (Windows NT 5.1; U; en)",
-    #     "Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.50",
-    #     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 9.50",
-    #     "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0",
-    #     ]
-    # headers['User-Agent'] = random.choice(user_agent_list)
-    # 添加异常处理
+    # try ... catch ...
     try:
         req = urllib.request.Request(url)
         req.add_header('Authorization', 'APPCODE ' + appcode)
-        # //根据API的要求，定义相对应的Content-Type
         req.add_header('Content-Type', 'application/json; charset=UTF-8')
         ctx = ssl.create_default_context()
         ctx.check_hostname = False
         ctx.verify_mode = ssl.CERT_NONE
         response = request.urlopen(req, context=ctx)
         content = response.read().decode()
-        # get json
         # parse json
         if (content):
             content1 = json.loads(content)
@@ -93,7 +91,7 @@ for card_num in data:
         print(err)
     finally:
         execute_times += 1
-workbook.save('D:/software/Desktop/银行卡/not_match_search_result_7.xls')
+workbook.save('./not_match_search_result_7.xls')
 
 
 
